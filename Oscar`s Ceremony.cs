c961e0ac33a09a22752d0b406956f6a55c2a1131
Inesa Kostova <kostovainesa@gmail.com>
2025-03-17 15:55:55 +0200

    Add files via upload

---
+++ Oscar`s Ceremony.cs
@@ -0,0 +1,8 @@
+﻿int rent = int.Parse(Console.ReadLine());
+
+double statues = rent-(rent*0.3);
+double food = statues - (statues * 0.15);
+double music = food / 2;
+double finalSum =  rent +statues+food+music;
+
+Console.WriteLine($"{finalSum:F2}");
