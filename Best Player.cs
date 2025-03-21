fff181c0c1690f89af472d09eee36fe5e07634ea
Inesa Kostova <kostovainesa@gmail.com>
2025-03-21 15:10:19 +0200

    Add files via upload

---
+++ Best Player.cs
@@ -0,0 +1,31 @@
+﻿string bestPlayer = "";
+int bestGoals = 0;
+while(true)
+{
+ string name = Console.ReadLine();
+if(name=="END")
+    {
+        break;
+    }
+int goals = int.Parse(Console.ReadLine());
+    if(goals > bestGoals)
+    {
+        bestPlayer= name;
+        bestGoals = goals;
+    }
+    if (goals >= 10)
+    {
+        break;
+    }
+
+}
+Console.WriteLine($"{bestPlayer} is the best player!");
+if (bestGoals >= 3)
+{
+    Console.WriteLine($"He has scored {bestGoals} goals and made a hat-trick !!!");
+}
+else
+{
+    Console.WriteLine($"He has scored {bestGoals} goals.");
+}
+
